7df63c47d664e2d7258d95e1fbaec344e80b748c
Samuel Padgett <spadgett@redhat.com>
2019-12-06 11:15:30 -0500

    Update available channels to 4.4

--- frontend/public/module/k8s/cluster-settings.ts
+++ frontend/public/module/k8s/cluster-settings.ts
@@ -28,8 +28,8 @@ export const getAvailableClusterUpdates = (cv: ClusterVersionKind): ClusterUpdat
 };
 
 export const getAvailableClusterChannels = () => ({
-  'stable-4.3': 'stable-4.3',
-  'fast-4.3': 'fast-4.3',
+  'stable-4.4': 'stable-4.4',
+  'fast-4.4': 'fast-4.4',
 });
 
 export const getDesiredClusterVersion = (cv: ClusterVersionKind): string => {
